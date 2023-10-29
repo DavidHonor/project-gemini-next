7c25605f5f87633a6bc912f60777596fdb911780
Kovács Levente <41201589+DavidHonor@users.noreply.github.com>
2023-10-29 09:18:11 +0100

    reorgonize env

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,10 +1,14 @@
 import * as admin from "firebase-admin";
 
-var serviceAccount = require("serviceAccountKey.json");
+//var serviceAccount = require("serviceAccountKey.json");
 
 if (!admin.apps.length) {
     admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
+        credential: admin.credential.cert({
+            projectId: process.env.PROJECT_ID,
+            clientEmail: process.env.CLIENT_EMAIL,
+            privateKey: process.env.PRIVATE_KEY!.replace(/\\n/g, "\n"),
+        }),
         storageBucket: "project-gemini-next.appspot.com",
     });
 }
