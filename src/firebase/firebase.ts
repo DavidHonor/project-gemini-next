ef003933294919ed57d3d45d67c78be51d51cc3c
Kovács Levente <41201589+DavidHonor@users.noreply.github.com>
2023-10-29 14:48:37 +0100

    fix key 2

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -10,7 +10,9 @@ if (!admin.apps.length) {
         credential: admin.credential.cert({
             projectId: process.env.FIREBASE_PROJECT_ID,
             clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-            privateKey: JSON.parse(process.env.FIREBASE_PRIVATE_KEY),
+            privateKey: JSON.parse(
+                process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+            ),
         }),
         storageBucket: "project-gemini-next.appspot.com",
     });
