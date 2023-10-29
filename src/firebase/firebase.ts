c21f22ebafccde86f1932c10f1296421d07fa794
Kovács Levente <41201589+DavidHonor@users.noreply.github.com>
2023-10-29 14:43:06 +0100

    fix key

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -2,12 +2,15 @@ import * as admin from "firebase-admin";
 
 //var serviceAccount = require("serviceAccountKey.json");
 
+if (!process.env.FIREBASE_PRIVATE_KEY)
+    throw new Error("Add firebase private key");
+
 if (!admin.apps.length) {
     admin.initializeApp({
         credential: admin.credential.cert({
             projectId: process.env.FIREBASE_PROJECT_ID,
             clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-            privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
+            privateKey: JSON.parse(process.env.FIREBASE_PRIVATE_KEY),
         }),
         storageBucket: "project-gemini-next.appspot.com",
     });
